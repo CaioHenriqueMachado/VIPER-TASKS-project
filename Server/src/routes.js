d69c8f9646f0767cd11e47b05c869add02f0e2d6
Caio <caio.student@outlook.com>
2020-06-25 20:51:39 -0300

    ajuste de pastas

---
+++ Server/src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get('/user', (request, response) => {
+    return response.json({
+        event: 'Semana',
+        aluno: 'Caio'
+    });
+});
+
+module.exports = routes
