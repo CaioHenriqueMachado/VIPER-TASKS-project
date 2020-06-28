adacd0f70e7639bb9393c163c5a9c64d6fb74fd6
Caio <caio.student@outlook.com>
2020-06-28 01:58:37 -0300

    ajuste conection register

--- web/src/pages/Register/index.js
+++ web/src/pages/Register/index.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
-
 import logoImg from '../../assests/logo1.svg';
 
+import api from '../../services/api';
+
 export default function Register() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+
+    const history = useHistory();
+    async function handleRegister(e){
+        e.preventDefault();
+
+        const data = {
+            name,
+            email,
+            login,
+            password
+        };
+
+        try {
+            const response =  await api.post('users', data);
+            alert ('Cadastro realizado com sucesso');
+            history.push('/');
+        } catch(err){
+            alert ('Erro no cadastro, tente novamente')
+        }
+        
+
+        
+    }
+
+
     return (
         <div className="register-container">
             <div className="content">
@@ -22,18 +52,28 @@ export default function Register() {
                     </Link>
 
                 </section>
-                <form>
+                <form onSubmit={handleRegister}>
                     <input 
-                    placeholder="Nome"/>
-
-                    <input type="email" placeholder="E-mail"
-       
+                        placeholder="Nome"
+                        value={name}
+                        onChange={ e => setName(e.target.value) }
+                    />                 
+                    <input 
+                        type="email" 
+                        placeholder="E-mail"
+                        value={email}
+                        onChange={ e => setEmail(e.target.value) }
                     />
-                    <input placeholder="Login" 
-             
+                    <input 
+                        placeholder="Login" 
+                        value={login}
+                        onChange={ e => setLogin(e.target.value) }
                     />
-              
-                    <input placeholder="Senha" />
+                    <input 
+                        placeholder="Senha" 
+                        value={password}
+                        onChange={ e => setPassword(e.target.value) }
+                    />
 
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
