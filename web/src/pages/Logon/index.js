d64333318df7463e4ff54fad72bd900c5d536282
Caio <caio.student@outlook.com>
2020-07-06 21:47:51 -0300

    Tela logon responsiva

--- web/src/pages/Logon/index.js
+++ web/src/pages/Logon/index.js
@@ -4,78 +4,81 @@ import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
 
 import logoImg from '../../assests/logo1.svg';
-import ideasImg from '../../assests/ideas.png';
+import wallpaperLogon from '../../assests/wallpaperLogon.png';
 
 import api from '../../services/api';
 
 import Error from '../../Error';
 
 export default function Logon() {
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
-    const [validate, setValidate] = useState(false);
+	const [login, setLogin] = useState('');
+	const [password, setPassword] = useState('');
+	const [validate, setValidate] = useState(false);
 
-    const message = 'Login ou senha inválida!!';
+	const message = 'Login ou senha inválida!!';
 
-    const history = useHistory();
+	const history = useHistory();
 
-    async function handleLogon(e){
-        e.preventDefault();
+	async function handleLogon(e){
+			e.preventDefault();
 
-        const data = {
-            login,
-            password
-        };
+			const data = {
+					login,
+					password
+			};
 
-        try {
-            const response =  await api.post('session', data);
-            localStorage.setItem('userName', response.data.name);
-            localStorage.setItem('userId', response.data.id);
+			try {
+					const response =  await api.post('session', data);
+					localStorage.setItem('userName', response.data.name);
+					localStorage.setItem('userId', response.data.id);
 
-            history.push('/profile');
-        } catch(err){
-            setValidate(true);
-            setValidate(false);
-            
-        }
+					history.push('/profile');
+			} catch(err){
+					setValidate(true);
+					setValidate(false);
+					
+			}
 
-        
-    }
-    return(
-        <>
-        <div className="logon-container">
-            <img src={logoImg} alt="logo" className="logo"/>
-            <section className="form">
-                <form onSubmit={handleLogon}>
-                    <h1>Faça seu Login</h1>
-                    <input 
-                        placeholder="Login"
-                        value={login}
-                        onChange={ e => setLogin(e.target.value) }
-                        minLength='8'
-                        maxLength='20'
-                    />
-                    <input 
-                        placeholder="Password" 
-                        type="password"
-                        value={password}
-                        onChange={ e => setPassword(e.target.value) }
-                        minLength='8'
-                        maxLength='20'
-                    />
-                    <button className="button" type="submit">Entrar</button>
-                    <Link to="/register" className="back-link">
-                        <FiLogIn size={16} color="#0609be"/>
-                        Não tenho cadastro
-                    </Link>
-                </form>
-            </section>
-            <div className="painel">
-                <img src={ideasImg} alt="painel" width={300}/>
-            </div>
-        </div>
-        <Error message={message} validate={validate} />
-        </>
-    );
+			
+	}
+	return(
+			<>
+			<div className="logon-container">
+					<img src={logoImg} alt="logo" className="logo"/>
+					<section className="form">
+					<h1>Faça seu Login</h1>
+							<form onSubmit={handleLogon}>
+									
+									<br></br>
+									<input 
+											placeholder="Login"
+											value={login}
+											onChange={ e => setLogin(e.target.value) }
+											minLength='8'
+											maxLength='20'
+									/>
+									<input 
+											placeholder="Password" 
+											type="password"
+											value={password}
+											onChange={ e => setPassword(e.target.value) }
+											minLength='8'
+											maxLength='20'
+									/>
+									<button className="button" type="submit">Entrar</button>
+									
+							</form>
+							<Link to="/register" className="back-link">
+											<FiLogIn size={16} color="#0609be"/>
+											Não tenho cadastro
+							</Link>
+					</section>
+					<div className="painel-logon">
+							<img src={wallpaperLogon} alt="painel" width={300}/>
+					</div>
+			</div>
+			<Error message={message} validate={validate} />
+			</>
+	);
 
 }
