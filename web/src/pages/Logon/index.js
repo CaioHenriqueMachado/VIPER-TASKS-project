77c35d7f6b3e36d4f6d493f9a26c78a4d6e6327e
Caio <caio.student@outlook.com>
2020-07-06 21:52:03 -0300

    identação no arquivo index logon

--- web/src/pages/Logon/index.js
+++ web/src/pages/Logon/index.js
@@ -20,65 +20,60 @@ export default function Logon() {
 	const history = useHistory();
 
 	async function handleLogon(e){
-			e.preventDefault();
+		e.preventDefault();
 
-			const data = {
-					login,
-					password
-			};
+		const data = {
+			login,
+			password
+		};
 
-			try {
-					const response =  await api.post('session', data);
-					localStorage.setItem('userName', response.data.name);
-					localStorage.setItem('userId', response.data.id);
+		try {
+			const response =  await api.post('session', data);
+			localStorage.setItem('userName', response.data.name);
+			localStorage.setItem('userId', response.data.id);
+			history.push('/profile');
 
-					history.push('/profile');
-			} catch(err){
-					setValidate(true);
-					setValidate(false);
-					
-			}
-
-			
+		}	catch(err) {
+				setValidate(true);
+				setValidate(false);
+		}
+		
 	}
-	return(
-			<>
-			<div className="logon-container">
-					<img src={logoImg} alt="logo" className="logo"/>
-					<section className="form">
-					<h1>Faça seu Login</h1>
-							<form onSubmit={handleLogon}>
-									
-									<br></br>
-									<input 
-											placeholder="Login"
-											value={login}
-											onChange={ e => setLogin(e.target.value) }
-											minLength='8'
-											maxLength='20'
-									/>
-									<input 
-											placeholder="Password" 
-											type="password"
-											value={password}
-											onChange={ e => setPassword(e.target.value) }
-											minLength='8'
-											maxLength='20'
-									/>
-									<button className="button" type="submit">Entrar</button>
-									
-							</form>
-							<Link to="/register" className="back-link">
-											<FiLogIn size={16} color="#0609be"/>
-											Não tenho cadastro
-							</Link>
-					</section>
-					<div className="painel-logon">
-							<img src={wallpaperLogon} alt="painel" width={300}/>
-					</div>
-			</div>
-			<Error message={message} validate={validate} />
-			</>
-	);
 
+return(
+	<>
+		<div className="logon-container">
+			<img src={logoImg} alt="logo" className="logo"/>
+			<section className="form">
+				<h1>Faça seu Login</h1>
+				<form onSubmit={handleLogon}>
+					<br></br>
+					<input 
+						placeholder="Login"
+						value={login}
+						onChange={ e => setLogin(e.target.value) }
+						minLength='8'
+						maxLength='20'
+					/>
+					<input 
+						placeholder="Password" 
+						type="password"
+						value={password}
+						onChange={ e => setPassword(e.target.value) }
+						minLength='8'
+						maxLength='20'
+					/>
+					<button className="button" type="submit">Entrar</button>
+				</form>
+				<Link to="/register" className="back-link">
+					<FiLogIn size={16} color="#0609be"/>Não tenho cadastro
+				</Link>
+			</section>
+			<div className="painel-logon">
+				<img src={wallpaperLogon} alt="painel" width={300}/>
+			</div>
+		</div>
+		<Error message={message} validate={validate} />
+	</>
+	);
 }
