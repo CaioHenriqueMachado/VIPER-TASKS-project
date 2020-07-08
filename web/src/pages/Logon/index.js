f74b03e7f4ff0a79196f15097caf8dd6dc31421d
Caio <caio.student@outlook.com>
2020-07-08 18:59:08 -0300

    ajuste de botão

--- web/src/pages/Logon/index.js
+++ web/src/pages/Logon/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiUser, FiLock } from 'react-icons/fi';
 import './styles.css';
 
 import logoImg from '../../assests/logo1.svg';
+import avatarImg from '../../assests/avatar.svg';
 import wallpaperLogon from '../../assests/wallpaperLogon.png';
 
 import api from '../../services/api';
@@ -42,7 +43,64 @@ export default function Logon() {
 
 return(
 	<>
-		<div className="body">
+	<div className="body">
+			<div className="logon-container">
+				{/* <img src={logoImg} alt="logo" className="logo"/> */}
+				<section className="form">
+					<img src={avatarImg} className="avatar-logon" alt="" width="100px"/>
+					<h1>Seja bem vindo</h1>
+					<form onSubmit={handleLogon}>
+						<div className="input-div one">
+							<div className="icon-div">
+							<FiUser className="icon" size={16} color="#0609be"/>
+							</div>
+							<div>
+								<h5>Usuário</h5>
+								<input 
+									id="inputUser"
+									className="input"
+									value={login}
+									onChange={ e => setLogin(e.target.value) }
+									minLength='8'
+									maxLength='20'
+								/>
+							</div>
+						</div>
+						<br></br><br></br><br></br>
+						<div class="input-div two">
+							<div class="icon-div">
+							<FiLock className="icon" size={16} color="#0609be"/>
+							</div>
+							<div>
+								<h5>Senha</h5>
+								<input 
+									className="input"
+									type="password"
+									value={password}
+									onChange={ e => setPassword(e.target.value) }
+									minLength='8'
+									maxLength='20'
+								/>
+							</div>
+						</div>
+						<button className="login-button">
+          		Entrar
+          		<span></span>
+        		</button>
+					</form>
+					<Link to="/register" className="back-link">
+						<FiLogIn size={16} color="#39ff14"/>Não tenho cadastro
+					</Link>
+				</section>
+				<div className="painel-logon">
+					<img src={wallpaperLogon} alt="painel" width={300}/>
+				</div>
+			</div>
+			<Error message={message} validate={validate} />
+		</div>
+			
+
+		{/* <div className="body">
 			<div className="logon-container">
 				<img src={logoImg} alt="logo" className="logo"/>
 				<section className="form">
@@ -75,7 +133,7 @@ return(
 				</div>
 			</div>
 			<Error message={message} validate={validate} />
-		</div>
+		</div> */}
 	</>
 	);
 }
