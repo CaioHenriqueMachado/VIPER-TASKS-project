765cd6978c23a3efe82389a0a8b067958cbbf63d
Caio <caio.student@outlook.com>
2020-07-09 22:36:00 -0300

    ajutes editar perfil

--- web/src/pages/EditProfile/index.js
+++ web/src/pages/EditProfile/index.js
@@ -3,7 +3,6 @@ import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn, FiKey } from 'react-icons/fi';
 import api from '../../services/api';
 
-
 import './styles.css';
 import avatarImg from '../../assests/avatar.svg';
 import editProfileImg from '../../assests/editProfileImg.svg';
@@ -22,20 +21,17 @@ export default function EditProfile() {
 	const userId = localStorage.getItem('userId');
 
 	useEffect(() => {
-        api.get('profile',{
-            headers: {
-                Authorization: userId,
-            }
-        }).then(response => {
-			setName(response.data.name)
-			setEmail(response.data.email)
-			setLogin(response.data.login)
-        })
+		api.get('profile',{
+			headers: {
+					Authorization: userId,
+			}
+		}).then(response => {
+		setName(response.data.name)
+		setEmail(response.data.email)
+		setLogin(response.data.login)
+		})
 	}, [userId]);
 	
-
-
-
 	async function handleProfileEdit(e){
 		e.preventDefault();
 
@@ -46,10 +42,10 @@ export default function EditProfile() {
 		};
 
 		try {
-            await api.put(`users/${userId}`,data, {
-                headers: {
-                    Authorization: userId,
-                }
+			await api.put(`users/${userId}`,data, {
+				headers: {
+					Authorization: userId,
+				}
 			});
 			localStorage.setItem('userName', data.name);
 			
@@ -57,10 +53,9 @@ export default function EditProfile() {
 			
 		} catch(err){
 			setValidate(true);
-            setValidate(false);
+				setValidate(false);
 		}
 	}
-	
 
 	return(
 		<>
@@ -97,69 +92,20 @@ export default function EditProfile() {
 								minLength='8'
 								maxLength='30'
 							/>
-
 					<Link to="/pwd" className="back-link">
 						<FiKey size={16} color="#0609be"/>
 						Alterar senha
 					</Link>
 					<button className="button" type="submit">Atualizar</button>
-					
 					<Link to="/profile" className="back-link">
 						<FiLogIn size={16} color="#0609be"/>
 						Voltar para tarefas
 					</Link>
 					</form>
 				</section>
-				<img src={editProfileImg} className='editProfile'  alt="painel"/>
-		
+				<img src={editProfileImg} className='editProfile'  alt="Edit Profile"/>
 				<Error message={message} validate={validate} />
 			</div>
-
-		{/* <div className="logon-container">
-			<img src={logoImg} alt="logo" className="logo"/>
-			<section className="form">
-				<form onSubmit={handleProfileEdit}>
-					<h1>Edite seu perfil</h1>
-					<h2>Nome</h2>
-					<input 
-						id="loco"
-						value={name}
-						onChange={ e => setName(e.target.value) }
-                        required
-                        minLength='4'
-                        maxLength='15'
-					/>
-					<h2>E-mail</h2>
-					<input 
-
-						value={email}
-						onChange={ e => setEmail(e.target.value) }
-						required
-                        minLength='8'
-                        maxLength='30'
-					/>
-					<h2>Login</h2>
-					<input 
-
-						value={login}
-						onChange={ e => setLogin(e.target.value) }
-						required
-                        minLength='8'
-                        maxLength='30'
-					/>
-					<Link to="/pwd" className="back-link">
-						<FiKey size={16} color="#0609be"/>
-						Alterar senha
-					</Link>
-					<button className="button" type="submit">Atualizar</button>
-					
-					<Link to="/profile" className="back-link">
-						<FiLogIn size={16} color="#0609be"/>
-						Voltar para tarefas
-					</Link>
-				</form>
-			</section>
-		</div> */}
 		</>
 	)
 }
